154875676048e922d2b0bcb761dbdb538e1611fa
Merrin Kurian <merrin_kurian@intuit.com>
2015-02-08 17:50:57 +0530

    corrections

--- src/main/java/org/localmarket/util/Constants.java
+++ src/main/java/org/localmarket/util/Constants.java
@@ -2,7 +2,7 @@ package org.localmarket.util;
 
 public class Constants {
 	
-	public static final String INDEX = "localmarmet";
+	public static final String INDEX = "localmarket";
 	public static final String INTEREST_TYPE = "interest";
 	
 	public static final String ES_HOST = "http://localhost:9200" ;
